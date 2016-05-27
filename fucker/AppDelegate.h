571ed1ae52369a4b35951e192a2fe8cd798435c9
shuaizhihao <shuaizhihao@hotmail.com>
2016-05-27 18:13:50 +0800

    Initial Commit

---
+++ fucker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fucker
+//
+//  Created by Admin on 16/5/27.
+//  Copyright © 2016年 tp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fucker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fucker
+//
+//  Created by Admin on 16/5/27.
+//  Copyright © 2016年 tp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
